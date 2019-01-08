2989c5827a7cde08a7d223a7a56ac416067d81a6
BaleLin <bale.lin@oocl.com>
2019-01-09 00:23:36 +0800

    refactor the ReceiptHandler code

--- computePrice/src/main/java/ReceiptHandler.java
+++ computePrice/src/main/java/ReceiptHandler.java
@@ -1,10 +1,19 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReceiptHandler {
   private String stateCode;
   private ArrayList<ReceiptItem> receiptItemArray = new ArrayList();
-
+  private double totalWithoutTaxes;
+  private double discout = 0;
+  private double tax;
+  private double addMoneyWithTax;
+  private double reduceMoneyWithDiscount;
+  private double actualPrice;
+  private StringBuilder sb = new StringBuilder();
+  private Map<String,Double> stateCodeMap = new HashMap<String,Double>();
   public String getStateCode() {
     return stateCode;
   }
@@ -25,18 +34,8 @@ public class ReceiptHandler {
     this.receiptItemArray.add(receiptItem);
   }
   public String generateReceipt(){
-    double totalWithoutTaxes = 0;
-    StringBuilder sb = new StringBuilder();
-    for (ReceiptItem receiptItem:receiptItemArray){
-      String itemName = receiptItem.getItemName();
-      int quantity = receiptItem.getQuantity();
-      double unitPrice = receiptItem.getUnitPrice();
-      double singleTotalPrice = quantity*unitPrice;
-      totalWithoutTaxes = totalWithoutTaxes + singleTotalPrice;
-      sb.append(String.format("%s     %d   %.2f   %.2f\n",itemName,quantity,unitPrice,singleTotalPrice));
-    }
-    double discout = 0;
-    double tax = 0;
+
+    this.formatReceiptItem();
     if (totalWithoutTaxes>1000){
       discout = 0.03;
     }if(totalWithoutTaxes>5000){
@@ -48,26 +47,49 @@ public class ReceiptHandler {
     }if (totalWithoutTaxes>50000){
       discout = 0.15;
     }
-    if (stateCode.equals("UT")){
-      tax = 0.0685;
-    } if (stateCode.equals("NV")){
-      tax = 0.08;
-    } if (stateCode.equals("TX")){
-      tax = 0.0625;
-    } if (stateCode.equals("AL")){
-      tax = 0.04;
-    }if (stateCode.equals("CA")){
-      tax = 0.0825;
+    tax = getTaxRate(this.getStateCode());
+    addMoneyWithTax = calculateAddMoney();
+    reduceMoneyWithDiscount = calculateReduceMoney();
+    actualPrice = calculateActualPrice();
+
+    this.formatReceiptsPrice();
+
+   return sb.toString();
+  }
+  private double calculateAddMoney(){
+    return totalWithoutTaxes*tax;
+  }
+  private double calculateReduceMoney(){
+    return totalWithoutTaxes*discout;
+  }
+  private double calculateActualPrice(){
+    return totalWithoutTaxes+addMoneyWithTax-reduceMoneyWithDiscount;
+  }
+  private double getTaxRate(String stateCode){
+    stateCodeMap.put("UT",0.0685);
+    stateCodeMap.put("NV",0.08);
+    stateCodeMap.put("TX",0.0625);
+    stateCodeMap.put("AL",0.04);
+    stateCodeMap.put("CA",0.0825);
+    return stateCodeMap.get(stateCode);
+  }
+  private void formatReceiptItem() {
+    for (ReceiptItem receiptItem : receiptItemArray) {
+      String itemName = receiptItem.getItemName();
+      int quantity = receiptItem.getQuantity();
+      double unitPrice = receiptItem.getUnitPrice();
+      double singleTotalPrice = quantity * unitPrice;
+      totalWithoutTaxes = totalWithoutTaxes + singleTotalPrice;
+      sb.append(String.format("%s     %d   %.2f   %.2f\n", itemName, quantity, unitPrice, singleTotalPrice));
     }
-    double addMoneyWithTax = totalWithoutTaxes*tax;
-    double reduceMoneyWithDiscount = totalWithoutTaxes*discout;
-    double actualPrice = totalWithoutTaxes+addMoneyWithTax-reduceMoneyWithDiscount;
+  }
+  private void formatReceiptsPrice(){
     sb.append("\n"+"-----------------------------------------------------"+"\n");
     sb.append(String.format("Total without taxes                               %.2f\n",totalWithoutTaxes));
     sb.append(String.format("Discount %.2f%%                                   -%.2f\n",discout*100,reduceMoneyWithDiscount));
     sb.append(String.format("Tax %.2f%%                                        +%.2f\n",tax*100,addMoneyWithTax));
     sb.append("\n"+"-----------------------------------------------------"+"\n");
     sb.append(String.format("Total price                                      %.2f\n",actualPrice));
-    return sb.toString();
   }
+
 }
