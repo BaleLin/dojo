a54e33179118d97fa701ef0fd9dd9f52d2fdc60e
BaleLin <bale.lin@oocl.com>
2019-01-09 08:32:02 +0800

    Refector ReceiptHandler: extract getDiscount method

--- computePrice/src/main/java/ReceiptHandler.java
+++ computePrice/src/main/java/ReceiptHandler.java
@@ -1,20 +1,23 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ReceiptHandler {
+
   private String stateCode;
-  private ArrayList<ReceiptItem> receiptItemArray = new ArrayList();
+  private List<ReceiptItem> receiptItemArray = new ArrayList<ReceiptItem>();
   private double totalWithoutTaxes;
-  private double discout = 0;
+  private double discount = 0;
   private double tax;
   private double addMoneyWithTax;
   private double reduceMoneyWithDiscount;
   private double actualPrice;
   private StringBuilder sb = new StringBuilder();
-  private Map<String,Double> stateCodeMap = new HashMap<String,Double>();
-  public String getStateCode() {
+  private Map<String, Double> stateCodeMap = new HashMap<String, Double>();
+
+  private String getStateCode() {
     return stateCode;
   }
 
@@ -22,7 +25,7 @@ public class ReceiptHandler {
     this.stateCode = stateCode;
   }
 
-  public ArrayList<ReceiptItem> getReceiptItemArray() {
+  public List<ReceiptItem> getReceiptItemArray() {
     return receiptItemArray;
   }
 
@@ -30,23 +33,14 @@ public class ReceiptHandler {
     this.receiptItemArray = receiptItemArray;
   }
 
-  public void addItem(ReceiptItem receiptItem){
+  
+  public void addItem(ReceiptItem receiptItem) {
     this.receiptItemArray.add(receiptItem);
   }
-  public String generateReceipt(){
+
+  public String generateReceipt() {
 
     this.formatReceiptItem();
-    if (totalWithoutTaxes>1000){
-      discout = 0.03;
-    }if(totalWithoutTaxes>5000){
-      discout = 0.05;
-    }if(totalWithoutTaxes>7000){
-      discout = 0.07;
-    }if (totalWithoutTaxes>10000){
-      discout = 0.10;
-    }if (totalWithoutTaxes>50000){
-      discout = 0.15;
-    }
     tax = getTaxRate(this.getStateCode());
     addMoneyWithTax = calculateAddMoney();
     reduceMoneyWithDiscount = calculateReduceMoney();
@@ -54,25 +48,49 @@ public class ReceiptHandler {
 
     this.formatReceiptsPrice();
 
-   return sb.toString();
+    return sb.toString();
   }
-  private double calculateAddMoney(){
-    return totalWithoutTaxes*tax;
+
+  private void getDiscount() {
+    if (totalWithoutTaxes > 1000) {
+      discount = 0.03;
+    }
+    if (totalWithoutTaxes > 5000) {
+      discount = 0.05;
+    }
+    if (totalWithoutTaxes > 7000) {
+      discount = 0.07;
+    }
+    if (totalWithoutTaxes > 10000) {
+      discount = 0.10;
+    }
+    if (totalWithoutTaxes > 50000) {
+      discount = 0.15;
+    }
   }
-  private double calculateReduceMoney(){
-    return totalWithoutTaxes*discout;
+
+  private double calculateAddMoney() {
+    return totalWithoutTaxes * tax;
   }
-  private double calculateActualPrice(){
-    return totalWithoutTaxes+addMoneyWithTax-reduceMoneyWithDiscount;
+
+  private double calculateReduceMoney() {
+    getDiscount();
+    return totalWithoutTaxes * discount;
   }
-  private double getTaxRate(String stateCode){
-    stateCodeMap.put("UT",0.0685);
-    stateCodeMap.put("NV",0.08);
-    stateCodeMap.put("TX",0.0625);
-    stateCodeMap.put("AL",0.04);
-    stateCodeMap.put("CA",0.0825);
+
+  private double calculateActualPrice() {
+    return totalWithoutTaxes + addMoneyWithTax - reduceMoneyWithDiscount;
+  }
+
+  private double getTaxRate(String stateCode) {
+    stateCodeMap.put("UT", 0.0685);
+    stateCodeMap.put("NV", 0.08);
+    stateCodeMap.put("TX", 0.0625);
+    stateCodeMap.put("AL", 0.04);
+    stateCodeMap.put("CA", 0.0825);
     return stateCodeMap.get(stateCode);
   }
+
   private void formatReceiptItem() {
     for (ReceiptItem receiptItem : receiptItemArray) {
       String itemName = receiptItem.getItemName();
@@ -83,13 +101,14 @@ public class ReceiptHandler {
       sb.append(String.format("%s     %d   %.2f   %.2f\n", itemName, quantity, unitPrice, singleTotalPrice));
     }
   }
-  private void formatReceiptsPrice(){
-    sb.append("\n"+"-----------------------------------------------------"+"\n");
-    sb.append(String.format("Total without taxes                               %.2f\n",totalWithoutTaxes));
-    sb.append(String.format("Discount %.2f%%                                   -%.2f\n",discout*100,reduceMoneyWithDiscount));
-    sb.append(String.format("Tax %.2f%%                                        +%.2f\n",tax*100,addMoneyWithTax));
-    sb.append("\n"+"-----------------------------------------------------"+"\n");
-    sb.append(String.format("Total price                                      %.2f\n",actualPrice));
+
+  private void formatReceiptsPrice() {
+    sb.append("\n" + "-----------------------------------------------------" + "\n");
+    sb.append(String.format("Total without taxes                               %.2f\n", totalWithoutTaxes));
+    sb.append(String.format("Discount %.2f%%                                   -%.2f\n", discount * 100, reduceMoneyWithDiscount));
+    sb.append(String.format("Tax %.2f%%                                        +%.2f\n", tax * 100, addMoneyWithTax));
+    sb.append("\n" + "-----------------------------------------------------" + "\n");
+    sb.append(String.format("Total price                                      %.2f\n", actualPrice));
   }
 
 }
